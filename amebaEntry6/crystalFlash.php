df94d00c91574bd0512b361815b39856b59aaa08
taketakenokonoko <taketakenokonoko0202@yahoo.co.jp>
2017-09-18 19:52:12 +0900

    add new file

---
+++ amebaEntry6/crystalFlash.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="content-language" content="ja">
+	</head>
+	<script type="text/javascript">
+		var beforeSts;
+		window.onload = function(){
+			//0.3秒おきに、表示するクリスタルを変える
+			beforeSts = 0;//0が明るくなる過程、1が暗くなる過程
+			flashCrystal();
+		}
+		function flashCrystal(){
+			if(document.getElementById("crystal1_1").style.display=="block"){
+				beforeSts = 0;
+				console.log("1");
+				document.getElementById("crystal1_1").style.display="none";
+				document.getElementById("crystal1_2").style.display="block";
+				document.getElementById("crystal1_3").style.display="none";
+			}else if(document.getElementById("crystal1_2").style.display=="block"){
+				console.log("2");
+				if(beforeSts==0){
+					document.getElementById("crystal1_1").style.display="none";
+					document.getElementById("crystal1_2").style.display="none";
+					document.getElementById("crystal1_3").style.display="block";
+				}else{
+					document.getElementById("crystal1_1").style.display="block";
+					document.getElementById("crystal1_2").style.display="none";
+					document.getElementById("crystal1_3").style.display="none";
+				}
+			}else{
+				console.log("3");
+				beforeSts = 1;
+				document.getElementById("crystal1_1").style.display="none";
+				document.getElementById("crystal1_2").style.display="block";
+				document.getElementById("crystal1_3").style.display="none";
+			}
+			//0.3秒置いて、再帰処理
+			setTimeout("flashCrystal()",300);
+		}
+	</script>
+	<body>
+		<form name="MyForm" id="MyForm">
+			<div style="position:relative;"><!--相対位置指定することで、画像の上に画像や文字を重ねる-->
+				<input type="image" id="himekaImg" src="./img/himeka001.png" width="800" height="1066"/>
+				<input type="image" id="crystal1_1" src="./img/crystal_blue_1.png" width="32" height="64" style="position:absolute;top:200px;left:100px;">
+				<input type="image" id="crystal1_2" src="./img/crystal_blue_2.png" width="32" height="64" style="position:absolute;top:200px;left:100px;display:none;">
+				<input type="image" id="crystal1_3" src="./img/crystal_blue_3.png" width="32" height="64" style="position:absolute;top:200px;left:100px;display:none;">
+			</div>
+		</form>
+	</body>
+</html>
